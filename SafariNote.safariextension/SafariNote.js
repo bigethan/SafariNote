207609b69dd2186fa3da412fcb3dc355df455de7
bigethan <e@bigethan.com>
2010-07-31 12:29:48 -0700

    adding settings to allow user to specify the page to load in the iframe, as well as the size of the frame.  Simplified the interface.

--- SafariNote.safariextension/SafariNote.js
+++ SafariNote.safariextension/SafariNote.js
@@ -1,37 +1,36 @@
 function toggleSimpleFrame(msgEvent)
 {
     if (msgEvent.name == "BigethanSafariNote" && window == window.top) {
-        var wrapEl, wrap, data, hr, dataTitle, dataUrl, dataText, iframe, textSelection, textHolder;
+        var wrapEl, wrap, data, hr, dataTitle, dataUrl, dataText, iframe, iframeSrc, textSelection, textHolder;
         wrapEl = document.getElementById('bsn_wrap');
+        iframeSettings = msgEvent.message;
         if (!wrapEl) {
             //use dom methods, as using innerHTML causes selection to be lost
             wrap = document.createElement('div');
             wrap.id = 'bsn_wrap';
-            data = document.createElement('div');
-            data.id = 'bsn_data';
-            dataTitle = document.createElement('p');
-            dataTitle.id = 'bsn_dataTitle';
-            dataUrl = document.createElement('p');
-            dataUrl.id = 'bsn_dataUrl';
-            dataText = document.createElement('p');
-            dataText.id = 'bsn_dataText';
-            closeText = document.createElement('p');
-            closeText.id = 'bsn_closeText';
-            closeText.innerText = 'Click Outside Box To Close';
+            //data = document.createElement('div');
+            //data.id = 'bsn_data';
+            //dataTitle = document.createElement('p');
+            //dataTitle.id = 'bsn_dataTitle';
+            //dataUrl = document.createElement('p');
+            //dataUrl.id = 'bsn_dataUrl';
+            //dataText = document.createElement('p');
+            //dataText.id = 'bsn_dataText';
+            //closeText = document.createElement('p');
+            //closeText.id = 'bsn_closeText';
+            //closeText.innerText = 'Click Outside Box To Close';
             iframe = document.createElement('iframe');
             iframe.id = 'bsn_frame';
-            iframe.src = 'https://simple-note.appspot.com/index.html';
             
             //stick em all together
-            data.appendChild(dataTitle);
-            data.appendChild(dataUrl);
-            data.appendChild(dataText);
+            //data.appendChild(dataTitle);
+            //data.appendChild(dataUrl);
+            //data.appendChild(dataText);
             
-            wrap.appendChild(closeText);
-            wrap.appendChild(data);
+            //wrap.appendChild(closeText);
+            //wrap.appendChild(data);
             wrap.appendChild(iframe);
             
-                        
             wrap.onclick = function() {
                 if (this.style.display == 'block') {
                     this.style.display = 'none';
@@ -40,11 +39,14 @@ function toggleSimpleFrame(msgEvent)
             wrapEl = wrap;
             document.body.appendChild(wrapEl);
         }
+        document.getElementById('bsn_frame').src = iframeSettings.url;
+        document.getElementById('bsn_frame').style.width = iframeSettings.width + 'px';
+        document.getElementById('bsn_frame').style.height = iframeSettings.height + 'px';
         //toggle it's visibility
         if(!wrapEl.style.display || wrapEl.style.display == 'none') {
             // add the note information to the data block
             //url, page title, highlighted text
-            document.getElementById('bsn_dataUrl').innerHTML = window.location;
+            /*document.getElementById('bsn_dataUrl').innerHTML = window.location;
             document.getElementById('bsn_dataTitle').innerHTML = document.title;
             textSelection = '';
             if (document.getSelection) {
@@ -56,7 +58,7 @@ function toggleSimpleFrame(msgEvent)
                 textHolder.innerHTML = textSelection;
             } else {
                 textHolder.style.display = 'none';
-            }
+            }*/
             wrapEl.style.display = 'block';
         } else {
             wrapEl.style.display = 'none';
